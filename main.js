f882e46d3171ea14823e6d735fea78c9dc6e9f0d
yang132 <31993113+yang132@users.noreply.github.com>
2019-04-21 16:41:23 -0400

    Test firebase function

--- main.js
+++ main.js
@@ -5,4 +5,13 @@
       function JoinFunction() { 
         document.getElementById("demo").innerHTML = "Attempting to Join";
       }
+
+function createUser() {
+	var uName = "bob";
+	var pass = "ross";
+	firebase.database().ref('users/' + uName).set( {
+		username: uName,
+		password: pass
+	});
+}
  
