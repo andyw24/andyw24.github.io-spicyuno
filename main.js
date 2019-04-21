ef5946c0349546061893d63337bfe92205a3c9e5
WalterRobinson <33521741+WalterRobinson@users.noreply.github.com>
2019-04-21 16:11:41 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 
       function CreateFunction() { 
-        document.getElementById("demo").innerHTML = <form action="/action_page.php">
-            Box Name: <input type="text" name="BoxName" value="Idea of sorts"><br>
-            <input type="submit" value="Submit">
-        </form>;
+        document.getElementById("demo").innerHTML = "huh";
       }
       function JoinFunction() { 
         document.getElementById("demo").innerHTML = "Attempting to Join";
